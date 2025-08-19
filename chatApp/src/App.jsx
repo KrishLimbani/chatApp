c5c2c9bce4b94a6532b17c315616f7c7e2ea268d
KrishLimbani <limbanikrish45@gmail.com>
2025-08-19 18:25:08 +0530

    for check

--- chatApp/src/App.jsx
+++ chatApp/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl font-medium text-center text-blue-500">Chat app</h1>
-      <h1 className="text-4xl font-medium text-center text-blue-500">Chat app</h1>
+
 
     </>
   )
