1e5a8c0de2e51c455716e077cd26ab5465e54e14
Paavan05 <paavanhn13@gmail.com>
2025-08-19 18:32:40 +0530

    demo

--- chatApp/src/App.jsx
+++ chatApp/src/App.jsx
@@ -1,3 +1,4 @@
+import Demo from "./components/demo"
 
 
 function App() {
@@ -6,8 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl font-medium text-center text-blue-500">Chat app</h1>
-
-
+      <Demo />
     </>
   )
 }
