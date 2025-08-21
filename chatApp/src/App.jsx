0e3c14acea81e573f8ff1b999b2cf44b17a5825a
KrishLimbani <limbanikrish45@gmail.com>
2025-08-21 10:43:08 +0530

    demo

--- chatApp/src/App.jsx
+++ chatApp/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
       <h1 className="text-4xl font-medium text-center text-blue-500">Chat app</h1>
-      <Demo />
+      <Demo/>
+      <h1>chatapp</h1>
     </>
   )
 }
